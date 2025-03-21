755a51190669226beea7947e199a3a6cfd42525b
Iván Recasens <irecasensl@gmail.com>
2025-03-21 22:22:06 +0100

    CH2L6 Count characters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_all_characters
 
 def get_book_text(path):
 
@@ -13,7 +14,7 @@ def main():
     book_content = get_book_text('books/frankenstein.txt')
     word_count = count_words(book_content)
     print(word_count, 'words found in the document')
-
+    print(count_all_characters(book_content))
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def count_words(content: str):
 
     return len(content.split())
+
+
+def count_all_characters(content: str) -> dict:
+
+    word_count = {}
+    for letter in content.lower():
+        word_count[letter] = word_count.setdefault(letter, 0) + 1
+
+    return word_count
