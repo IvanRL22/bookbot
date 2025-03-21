d71b0bd32b92761f2189535643a67f778f1376f4
Iván Recasens <irecasensl@gmail.com>
2025-03-21 22:54:45 +0100

    CH3L1 Print a report

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from stats import count_words
+from stats  import count_words
 from stats import count_all_characters
+from stats import sort_dict
 
 def get_book_text(path):
+
+    print('Analyzing book found at', path, '...')
 
     with open(path, 'r') as f:
         file_contents = f.read()
@@ -9,12 +12,22 @@ def get_book_text(path):
     return file_contents
 
 
-
 def main():
+
+    print('============ BOOKBOT ============')
+
     book_content = get_book_text('books/frankenstein.txt')
+
     word_count = count_words(book_content)
-    print(word_count, 'words found in the document')
-    print(count_all_characters(book_content))
+    print('----------- Word Count ----------')
+    print('Found', word_count, 'total words')
+
+    print('--------- Character Count -------')
+    for d in sort_dict(count_all_characters(book_content)):
+        if str(d['character']).isalpha():
+            print(d['character'], d['count'], sep=': ')
+
+    print('============= END ===============')
 
 
 main()
--- stats.py
+++ stats.py
@@ -3,10 +3,21 @@ def count_words(content: str):
     return len(content.split())
 
 
-def count_all_characters(content: str) -> dict:
+def count_all_characters(content: str) -> dict[str, int]:
 
     word_count = {}
     for letter in content.lower():
         word_count[letter] = word_count.setdefault(letter, 0) + 1
 
     return word_count
+
+
+def sort_dict(input_dict: dict[str, int]) -> list[dict]:
+
+    all_dicts = list()
+    for letter in input_dict.keys():
+        all_dicts.append({'character': letter, 'count': input_dict[letter]})
+
+    all_dicts.sort(key=lambda d: d['count'], reverse=True)
+    return all_dicts
+
