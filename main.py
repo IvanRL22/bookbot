c9f586e7c9bf2d9d2df984af524466dd6184220e
Iván Recasens <irecasensl@gmail.com>
2025-03-21 22:07:40 +0100

    CH2L4 Count words

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ def get_book_text(path):
     return file_contents
 
 
+def count_words(content: str):
+
+    return len(content.split())
+
+
 def main():
-    print(get_book_text('books/frankenstein.txt'))
+    book_content = get_book_text('books/frankenstein.txt')
+    word_count = count_words(book_content)
+    print(word_count, 'words found in the document')
+
 
 
 main()
